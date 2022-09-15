135bc21c488a0402dd37f5d7d7a76cee628023e7
Steven Croft <scroft001@gmail.com>
2022-09-14 21:28:37 -0700

    axios and youtube api

--- youtubeclone/src/apis/youtube.js
+++ youtubeclone/src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyBuF4TUmIvwmBSuEb6S6P8SHDv-G3rpQZA'
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part:'snippet',
+        type: 'video',
+        maxResults: 5,
+        key: KEY
+    }
+})
--- youtubeclone/src/components/SearchBar.js
+++ youtubeclone/src/components/SearchBar.js
@@ -10,8 +10,7 @@ class SearchBar extends React.Component {
 
     onFormSubmit = (event) => {
         event.preventDefault();
-
-        // ToDo: make sure we call the callback from parent component
+        this.props.onFormSubmit(this.state.term);
     }
 
     render() {
