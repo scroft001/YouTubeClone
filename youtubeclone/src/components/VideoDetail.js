b931a0a24328d6149dcc9ba0d9a6d6d826761216
Steven Croft <scroft001@gmail.com>
2022-09-16 21:46:39 -0700

    Conditional rendering

--- youtubeclone/src/components/VideoDetail.js
+++ youtubeclone/src/components/VideoDetail.js
@@ -1,2 +1,16 @@
 import React from 'react';
 
+const VideoDetail = ({video}) => {
+
+    if(!video){
+        return <div>Loading...</div>
+    }
+
+    return (
+        <div>
+            {video.snippet.title}
+        </div>
+    )
+}
+
+export default VideoDetail;
